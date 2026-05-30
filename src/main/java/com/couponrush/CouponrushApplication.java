920357dcf329a5d3f506da54c1f4a99ad6fb66dc
이건호 <dlrjsgh367@gmail.com>
2026-05-30 13:31:45 +0900

    Chore: Spring Boot 3.5.14 프로젝트 셋업 (의존성, application.yml)

---
+++ src/main/java/com/couponrush/CouponrushApplication.java
@@ -0,0 +1,13 @@
+package com.couponrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponrushApplication.class, args);
+	}
+
+}
